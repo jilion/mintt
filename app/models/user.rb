32808834ae9e758b2b114fb393b4f11457e6cd64
Octave Zangs <octavez@octavez.com>
2010-03-02 12:46:26 +0100

    Updated look&feel of the register form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User
   include MongoMapper::Document
   
-  key :gender, Boolean, :required => true
-  key :first_name, String, :required => true
-  key :last_name, String, :required => true
-  key :faculty, String, :required => true
+  attr_accessor :agreement
+  
+  key :gender, String
+  key :first_name, String
+  key :last_name, String
+  key :faculty, String
   key :email, String
-  key :phone, String, :required => true
+  key :phone, String
   key :url, String
   key :linkedin_url, String
-  key :thesis_supervisor, String, :required => true
-  key :thesis_subject, String, :required => true
-  key :supervisor_authorization, Boolean, :require => true
-  key :doctoral_school_rules, Boolean, :require => true
+  key :thesis_supervisor, String
+  key :thesis_subject, String
+  key :supervisor_authorization, String
+  key :doctoral_school_rules, String
   key :thesis_invention, String
   key :motivation, String
-  key :agreement, Boolean, :require => true
   
   timestamps!
   
@@ -24,9 +25,17 @@ class User
   # Email regex used to validate email formats. Retrieved from authlogic.
   EMAIL_REGEX = /\A[\w\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)\z/i
   
-  validates_presence_of :email
+  validates_presence_of :email, :first_name, :last_name, 
+                        :faculty, :phone, :thesis_supervisor, 
+                        :thesis_subject,
+                        :message => "This field can't be empty"
   validates_uniqueness_of :email, :allow_blank => true
   validates_format_of :email, :with => EMAIL_REGEX, :allow_blank => true
+  
+  validates_inclusion_of  :gender, :within => %w( male female ), :message => "Choose a gender"
+  validates_inclusion_of  :supervisor_authorization, :within => %w( yes no ), :message => "Choose an answer"
+  validates_inclusion_of  :doctoral_school_rules, :within => %w( yes no ), :message => "Choose an answer"
+  validates_acceptance_of :agreement
   
   # validates_presence_of :password, :if => :password_required?
   # validates_confirmation_of :password, :if => :password_required?
