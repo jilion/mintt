5d5638b88f399c506dfdb83e1533ab93a9dc05f5
Rémy Coutable <remy.coutable@gmail.com>
2010-03-04 15:52:57 +0100

    Making seeds.rb more rock solid

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
-
 require 'faker'
 
 TRUE_FALSE = %w(1 0)
 YES_NO = %w(yes no)
 
-
 case Rails.env
 when 'development'
+  development_seeds
+when 'test'
+  test_seeds
+when 'production'
+  production_seeds
+end
+
+
+def development_seeds
+  empty_tables
+  create_new_message_mail_template
+  create_users(87)
+  create_messages(46)
+end
+
+def test_seeds
+  empty_tables
+end
+
+def production_seeds
+  create_new_message_mail_template
+  create_users(87)
+  create_messages(46)
+end
+
+
+def empty_tables
   print "Deleting the content of MailTemplate, Message, User tables.. => "
   [MailTemplate, Message, User].each { |model| model.delete_all }
   print "empty!\n\n"
-  
-  print "Creating the 'New message' mail template => "
-  MailTemplate.create(:title => 'new_message', :content => "Dear {{user.first_name}} {{user.last_name}},\nwe've received your request for participating in the Mintt program.\n\nTo confirm you demand, please click on the link below :\n{{user.confirmation_link}}\n\n\nThanks for your interest in the Mintt program,\n\nthe whole Mintt team.")
-  print "created.\n\n"
-  
-  n = 87
+end
+
+def create_users(count)
   print "Creating users => "
-  n.times do |i|
+  count.times do |i|
     u = User.new
     u.gender = 'male'
     u.first_name = Faker::Name.first_name
@@ -45,11 +60,11 @@ when 'development'
     u.agreement = '1'
     u.save!
   end
-  print "#{n} users created.\n\n"
-  
-  n = 46
+  print "#{count} users created.\n\n"end
+
+def create_messages(count)
   print "Creating messages => "
-  n.times do |i|
+  count.times do |i|
     m = Message.new
     m.created_at = rand(10000).hours.from_now
     m.sender_name = Faker::Name.first_name
@@ -61,6 +76,11 @@ when 'development'
     m.replied = m.read? ? TRUE_FALSE.rand : '0'
     m.save!
   end
-  print "#{n} messages created.\n\n"
-  
+  print "#{count} messages created.\n\n"
+end
+
+def create_new_message_mail_template
+  print "Creating the 'New message' mail template => "
+  MailTemplate.create(:title => 'new_message', :content => "Dear {{user.first_name}} {{user.last_name}},\nwe've received your request for participating in the Mintt program.\n\nTo confirm you demand, please click on the link below :\n{{user.confirmation_link}}\n\n\nThanks for your interest in the Mintt program,\n\nthe whole Mintt team.")
+  print "created.\n\n"
 end
