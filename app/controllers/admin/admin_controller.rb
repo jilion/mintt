29d6dad0a0745d61053c18f0a2b7a2beff17aef2
Rémy Coutable <remy.coutable@gmail.com>
2010-03-05 17:31:31 +0100

    Redirecting from /admin to /admin/users

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,9 @@
 class Admin::AdminController < ApplicationController
   before_filter :admin_required
   layout 'admin'
+  
+  def index
+    redirect_to :controller => 'admin/users'
+  end
+  
 end
