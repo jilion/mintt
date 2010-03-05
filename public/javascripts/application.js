a1a3f85064efaf4f95cc56ece103875712b61a68
Octave Zangs <octavez@octavez.com>
2010-03-05 18:51:01 +0100

    Updated css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 document.observe("dom:loaded", function() {
   // If Windows Disable Typekit
-  if ((navigator.userAgent.indexOf("Windows")!=-1)) $(document.body).addClassName('winxp');
+  if ((navigator.userAgent.indexOf("Windows")!=-1)) $(document.body).addClassName('win');
   
   $$('tr.message').each(function(tr){
     tr.observe('click', function(e){
