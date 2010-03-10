d64b21acef08a834a9e94c3e5571b15ae9f18525
Rémy Coutable <remy.coutable@gmail.com>
2010-03-10 19:49:37 +0100

    Fixing untested helper (shame on me...)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,15 @@ module ApplicationHelper
   end
   
   def display_date(date)
-    date.strftime("%B %d, %Y")
+    date.strftime("%B %d, %Y") unless date.nil?
   end
   
   def display_date_and_time(date)
-    date.strftime("%B %d, %Y %I:%M %p")
+    date.strftime("%B %d, %Y %I:%M %p") unless date.nil?
   end
   
   def sexy_date(date)
+    return '' if date.nil?
     if date.today?
       "Today"
     elsif date.to_time > 2.days.until(Time.now)
@@ -24,6 +25,7 @@ module ApplicationHelper
   end
   
   def sexy_time(date)
+    return '' if date.nil?
     date.strftime("%I:%M %p")
   end
   
