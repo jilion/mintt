7ccc0c748a793a97e5a78e1ad4a3a9fa621e135d
Rémy Coutable <rymai@rymai.com>
2010-09-24 15:25:25 +0200

    Don't use fucking haml_tag in helpers (allowing us to always use = to display stuff in views)

--- app/helpers/google_analytics_helper.rb
+++ app/helpers/google_analytics_helper.rb
@@ -2,18 +2,17 @@ module GoogleAnalyticsHelper
   
   def google_analytics
     if Rails.env.production?
-      haml_tag :script, :type => "text/javascript" do
-        haml_concat("var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-10280941-6']);
-      _gaq.push(['_setDomainName', 'mintt.epfl.ch']);
-      _gaq.push(['_trackPageview']);
-      
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+      content_tag(:script, :type => "text/javascript") do
+        "var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-10280941-6']);
+        _gaq.push(['_setDomainName', 'mintt.epfl.ch']);
+        _gaq.push(['_trackPageview']);
+        
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
         })();"
-        )
       end
     end
   end
