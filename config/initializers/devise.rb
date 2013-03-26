0a2ec446bb87f686551358f9e560cf505b7e8102
Rémy Coutable <remy@jilion.com>
2013-03-26 11:11:36 +0100

    Use a more user-friendly email address in Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "mintt@epfl.ch"
+  config.mailer_sender = SiteSettings.mintt_sender
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
